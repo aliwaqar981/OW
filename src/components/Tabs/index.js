dbea5a0f9e1bf15073afcb54699eecd4d8bdc4bc
hassan <hassan0501651@gmail.com>
2023-07-24 10:52:08 +0500

    splash screen fix

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,11 +1,10 @@
-/* eslint-disable react-native/no-inline-styles */
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {FONTS} from '../../themes';
 
 const Tabs = ({ActiveTabColor, label, onPress, labelColor}) => {
   return (
-    <TouchableOpacity style={{width: '33.33%'}} onPress={onPress}>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
       <Text style={[styles.label, {color: labelColor}]}>{label}</Text>
       <View style={{height: 3, backgroundColor: ActiveTabColor}} />
     </TouchableOpacity>
@@ -15,6 +14,9 @@ const Tabs = ({ActiveTabColor, label, onPress, labelColor}) => {
 export default Tabs;
 
 const styles = StyleSheet.create({
+  container: {
+    width: '50%',
+  },
   label: {
     fontSize: 16,
     textAlign: 'center',
